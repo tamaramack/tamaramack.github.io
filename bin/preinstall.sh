846adbb86dda96a3c24521786d671c06fb9ccc99
Tamara Mack <tamaramack0508@gmail.com>
2017-01-26 17:17:19 -0800

    clean up npm link automation

--- bin/preinstall.sh
+++ bin/preinstall.sh
@@ -1,11 +1,2 @@
 #!/usr/bin/env bash
 
-npm -gf install npm@latest
-npm -gf install graceful-fs@latest
-npm -gf install minimatch@latest
-npm -gf install coffee-script@latest
-npm -gf install marked@latest
-npm -gf install jshint@latest
-npm -gf install node-gyp@latest
-npm -gf install node-sass@latest
-

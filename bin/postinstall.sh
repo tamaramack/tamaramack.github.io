61c1d8de8cc73a76ab1bfc57b3e25ae1faf3ab7e
Tamara Mack <tamaramack0508@gmail.com>
2017-11-13 15:41:01 -0800

    package updates

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./node_modules/bower/bin/bower cache clean
+# ./node_modules/bower/bin/bower cache clean
 ./node_modules/bower/bin/bower install --save
 ./node_modules/grunt/bin/grunt merge

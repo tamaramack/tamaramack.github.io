02883e266d1f2647695e79eb2e618b79d883d7b6
Tamara Mack <tamaramack0508@gmail.com>
2019-04-08 12:03:27 -0700

    added deploy files and scripts for heroku and github

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # ./node_modules/bower/bin/bower cache clean
-
+npm run build
+npm run build:dev
---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * server.js js file created by Tamara G. Mack on 08-Apr-19 for tamaramack.github.io
+ */
+const express = require('express');
+const serveStatic = require("serve-static");
+const path = require('path');
+
+const folder = (process.env.NODE_ENV === 'production') ? 'dist' : 'dev-build';
+
+app = express();
+app.use(serveStatic(path.join(__dirname, folder)));
+const port = process.env.PORT || 80;
+app.listen(port);

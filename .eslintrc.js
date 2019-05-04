a8989b3ab1a210bc2c6e3ddcd52eae2bcfecb8fb
Tamara Mack <tamaramack0508@gmail.com>
2019-05-03 17:58:24 -0700

    test lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,8 @@ module.exports = {
     '@vue/airbnb'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'func-names': 0,
     'no-restricted-syntax': 0,
     'guard-for-in': 0,
